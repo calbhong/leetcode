384a41c536aa376324544501de5322ac071c16b6
Cal Hong <calbhong@gmail.com>
2022-09-25 23:52:56 -0700

    Time: 122 ms (71.40%), Space: 51.5 MB (26.37%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    const map = {};
+    
+    for (const num of nums){
+        if(map[num])return true;
+        map[num] = true;
+    }
+    return false;
+};
